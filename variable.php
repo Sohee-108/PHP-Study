0f6a24f4d58f9070cc8da396d871a2506014c4ae
ddohing <dnflwlq004@naver.com>
2022-04-21 00:32:17 +0900

    변수

---
+++ variable.php
@@ -0,0 +1,26 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Variable</h1>
+    <?php
+    $a = 1; // $ - 변수 선언
+    echo $a+1;
+    ?>
+    <h2>test</h2>
+    <?php
+    $name = "hello~~";
+    echo "Lorem ipsum dolor sit, ".$name." amet consectetur adipisicing elit. Animi at placeat ".$name." dolor dolores. Distinctio
+    voluptatibus ex ".$name."
+    consequatur maxime illum dicta ".$name." dolorem commodi itaque officiis repudiandae. Voluptatum eum ex inventore
+    aspernatur." // lorem - 랜덤텍스트 생성
+    ?>
+</body>
+
+</html>
