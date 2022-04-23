02878a09707a2d73fd7c383ae2000ac7c03fe691
ddohing <dnflwlq004@naver.com>
2022-04-23 22:46:59 +0900

    URL 파라미터

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 <html>
 
 <body>
-    <?php 
-        echo date('Y-m-d H:i:s');
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php
+        echo $_GET['id'];
         ?>
+    </h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur excepturi inventore soluta, voluptates, optio, quos quae vitae iste eligendi repudiandae maiores! Sint sapiente, sequi blanditiis quibusdam qui maxime dicta voluptate.
 </body>
 
 </html>
