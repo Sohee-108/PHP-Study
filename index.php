bd3e068825be90df388df3e290e7f69056e712fe
ddohing <dnflwlq004@naver.com>
2022-04-23 23:51:40 +0900

    조건문과 반복문의 활용

--- index.php
+++ index.php
@@ -13,12 +13,13 @@
     </ol> -->
     <?php
     //data 디렉토리에 있는 파일의 목록을 가져온 후, li와 a 태그를 이용해서 글목록 만들기
-    $list = scandir('data');
+    $list = scandir('data'); //디렉토리를 스캔
     $i = 0;
     ?>
     <ol>
         <?php while($i<count($list)){
-            if($list[$i] != '.'){
+            //scandir은 .과 ..의 디렉토리도 포함
+            if($list[$i] != '.'){ 
                 if($list[$i] != '..'){
                     ?>
                     <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
