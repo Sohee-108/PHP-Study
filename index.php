0305eb77d3bb13e0bc9a93737ed0f76bf017c145
ddohing <dnflwlq004@naver.com>
2022-04-23 23:21:05 +0900

    조건문(if~else)

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html>
-
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
 <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
         <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
@@ -10,12 +13,20 @@
     </ol>
     <h2>
         <?php
+        if(isset($_GET['id'])){
         echo $_GET['id'];
+        }else {
+            echo "WELCOME";
+        }
         ?>
     </h2>
-    <?php 
-    echo file_get_contents("data/".$_GET['id']);
-    ?>
+        <?php 
+        if(isset($_GET['id'])){
+        echo file_get_contents("./data/".$_GET['id']);}
+        else{
+            echo "Hello, PHP";
+        }
+        ?>
 </body>
 
 </html>
